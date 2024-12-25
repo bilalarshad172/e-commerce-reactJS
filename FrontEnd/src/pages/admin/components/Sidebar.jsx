06a1e79b28004ece49d388b52b070d6c3df0a0e2
Bilal_Arshad <barshad172@gmail.com>
2024-12-25 16:38:59 +0500

    sidebar changes

--- FrontEnd/src/pages/admin/components/Sidebar.jsx
+++ FrontEnd/src/pages/admin/components/Sidebar.jsx
@@ -6,7 +6,16 @@ const Sidebar = () => {
     <>
       <Header />
       <div className="grid grid-cols-4">
-        <div className="col-span-1 border-r h-screen shadow-md">Sidebar</div>
+        <div className="col-span-1 border-r h-screen shadow-md">
+          <ul>
+            <li className="p-4 border-b">Dashboard</li>
+            <li className="p-4 border-b">Products</li>
+            <li className="p-4 border-b">Content Management</li>
+            <li className="p-4 border-b">Orders</li>
+            <li className="p-4 border-b">Users</li>
+            <li className="p-4 border-b">Settings</li>
+          </ul>
+        </div>
       </div>
     </>
   );
